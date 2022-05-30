d327b3245dda6542e2b7b3553adc578b43c336f5
Ben Chong <benchong1999@yahoo.com.au>
2022-05-30 21:15:37 +1000

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Start from '../Start';
+
+function App() {
+  return (
+    <Start />
+  );
+}
+
+export default App;
---
+++ src/components/Questions/index.js
@@ -0,0 +1,2 @@
+export * from './Questions';
+export { default } from './Questions';
